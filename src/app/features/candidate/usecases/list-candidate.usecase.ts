1f8fc4ee308f9e667e1cf1995a8bd040b6e76361
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-08 22:38:19 -0300

    criar, listar e deletar candidatos do Redis

--- src/app/features/candidate/usecases/list-candidate.usecase.ts
+++ src/app/features/candidate/usecases/list-candidate.usecase.ts
@@ -1,4 +1,5 @@
 import { UserType } from "../../../models/user-type.model";
+import { CacheRepository } from "../../../shared/database/repositories/cache.repository";
 import { Result } from "../../../shared/util";
 import { Usecase } from "../../../shared/util/usecase.contract";
 import { UserRepository } from "../../user/repositories/user.repository";
@@ -6,7 +7,24 @@ import { UserRepository } from "../../user/repositories/user.repository";
 export class ListCandidateUsecase implements Usecase {
   public async execute(): Promise<Result> {
     const repository = new UserRepository();
+    const cacheRepository = new CacheRepository();
+
+    const cacheResult = await cacheRepository.get("candidates");
+
+    if(cacheResult){
+      return{
+        ok: true,
+        message: "Candidates successfully listed in cache",
+        data: cacheResult,
+        code: 200,
+      }
+    }
+
     const result = await repository.list(UserType.Candidate);
+
+    const data = result?.map((candidate) => candidate.toJson())
+
+    await cacheRepository.set("candidate", data)
 
     return {
       ok: true,
