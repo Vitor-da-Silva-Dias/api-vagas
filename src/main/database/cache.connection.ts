07ad04a6b5cb3f0c940dbd59674ac76167c66f72
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-10 20:55:13 -0300

    comentários e ajustes no redis

--- src/main/database/cache.connection.ts
+++ src/main/database/cache.connection.ts
@@ -1,14 +1,17 @@
 import { Redis } from "ioredis";
 import config from "../config/redis.config";
 
-export class CacheDatabase{
-    private static _connection: Redis;
+export class CacheDatabase {
+  private static _connection: Redis;
 
-    public static async connect(){
-        this._connection = new Redis(config);
-    }
+  // CHAMA NO INDEX
+  public static async connect() {
+    this._connection = new Redis(config);
+    console.log("CacheDatabase is connected");
+  }
 
-    public static get connection(){
-        return this._connection;
-    }
+  // CHAMA NOS REPOSITORIOS
+  public static get connection() {
+    return this._connection;
+  }
 }
