9434b31f9f78e974b20b4fc625955b76a6262918
Vitor-da-Silva-Dias <diasdasilvavitor@gmail.com>
2023-08-10 21:01:19 -0300

    comentários e outros códigos de exemplo

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import { Database } from "./main/database/database.connection";
 import { Server } from "./main/server/express.server";
 import { CacheDatabase } from "./main/database/cache.connection";
 
+// AGUARDA TUDO SE RESOLVER
 Promise.all([Database.connect(), CacheDatabase.connect()]).then(() => {
-    console.log("Database is connected");
     Server.listen();
+    console.log("Feito");
 });
+
+// Database.connect().then(() => {
+//     console.log("Database is connected");
+//     Server.listen();
+// });
